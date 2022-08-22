2c72079a9ed812ac9e47b8954039527a54f66708
pilichm <michal.pilichowski@onet.pl>
2022-08-22 19:56:18 +0200

    Initial commit

---
+++ app/src/main/java/pl/pilichm/machinelearningdemo/MainActivity.kt
@@ -0,0 +1,15 @@
+package pl.pilichm.machinelearningdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import pl.pilichm.machinelearningdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
